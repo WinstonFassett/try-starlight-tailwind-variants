1ed145e86dbca3891fcb8f235e29b36de3d33a0c
Winston Fassett <winston.fassett@gmail.com>
2024-08-26 08:49:24 -0500

    shad button style

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,12 +1,27 @@
+import * as React from 'react'
 import { button } from './theme/button';
 import type { ButtonVariants } from './theme/button';
+import { cn } from '@/lib/utils';
+import { Slot } from "@radix-ui/react-slot"
 
 type ButtonHTMLAttributes = Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, 'size'>;
 
 export type ButtonProps = ButtonHTMLAttributes & ButtonVariants & {
+  asChild?: boolean
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
 };
 
-export function Button ({ children, onClick, ...props }: ButtonProps) {
-  return <button className={button(props)}>{children}</button>
-}
+export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, size, color, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button"
+    return (
+      <Comp
+        className={cn(button({ size, color }))}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
+Button.displayName = "Button"
+
